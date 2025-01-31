c78aca858e6e32a84cd6742da2e6eab10b91a5b8
luarakelly <lua.kellly@gmail.com>
2025-01-31 20:38:49 +0200

    Updated types and imports in serve.java

--- android/src/main/java/at/alwinschuster/HttpServer/Server.java
+++ android/src/main/java/at/alwinschuster/HttpServer/Server.java
@@ -18,10 +18,12 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.Map;
+import java.util.LinkedHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.CompletableFuture;
 import java.util.zip.GZIPInputStream;
+import java.nio.charset.StandardCharsets;
 
 import android.util.Log;
 
@@ -45,7 +47,7 @@ public class Server extends NanoHTTPD {
     public Server(ReactContext context, int port) {
         super(port);
         reactContext = context;
-        tileCache = new LRUCache<>(MAX_CACHE_SIZE);
+        tileCache = new LRUCache<String, byte[]>(MAX_CACHE_SIZE);
         tilesFile = new File(TILE_FILE_NAME);
 
         Log.d(TAG, "Server started");
