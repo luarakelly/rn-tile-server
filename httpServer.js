09680cb526e40deb02c7f95dffc8e3798ebcb622
Luara Kelly <108395474+luarakelly@users.noreply.github.com>
2025-02-01 18:13:17 +0200

    Update httpServer.js

--- httpServer.js
+++ httpServer.js
@@ -4,14 +4,15 @@ import { NativeModules } from "react-native";
 const Server = NativeModules.HttpServer;
 
 module.exports = {
-  start: function (port, bindAddress, serviceName) {
+  start: function (port, bindAddress, serviceName, callback) {
     if (port === 80) {
       throw "Invalid server port specified. Port 80 is reserved.";
     }
-    Server.start(port, bindAddress, serviceName);
+    Server.start(port, bindAddress, serviceName, callback);
   },
 
   stop: () => {
     Server.stop();
-  },
+  }
 };
+
